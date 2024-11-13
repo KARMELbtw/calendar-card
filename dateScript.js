319bd601f743822f4bab05f4bee80c68bcb4ca82
KARMELbtw <113383451+KARMELbtw@users.noreply.github.com>
2024-11-13 09:05:36 +0100

    Add files via upload

--- dateScript.js
+++ dateScript.js
@@ -6,6 +6,7 @@ let img = document.getElementById('img');
 let dayNumber = document.getElementById('dayNumber');
 let day = document.getElementById('day');
 let countdown = document.getElementById('countdown');
+let body = document.body;
 
 let month = Array('Styczeń', 'Luty', 'Marzec', 'Kwiecień', 'Maj', 'Czerwiec', 'Lipiec', 'Sierpień', 'Wrzesień', 'Październik', 'Listopad', 'Grudzień');
 let dayOfWeek = Array('Niedziela', 'Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota');
@@ -21,14 +22,51 @@ if (currentDate.getDate() < 10) {
 day.innerHTML = dayOfWeek[currentDate.getDay()];
 
 if (currentDate.getDay() === 0) {
-    dayNumber.style.color = 'red';
-    day.style.color = 'red';
+    dayNumber.style.color = 'rgb(219, 18, 18)';
+    day.style.color = 'rgb(219, 18, 18)';
 }
 
-while (currentDate >= birthdayDate) {
+while (currentDate > birthdayDate) {
     let year = birthdayDate.getFullYear();
     year++;
     birthdayDate.setFullYear(year);
 }
+switch (currentDate.getMonth()) {
+    case 11: case 0: case 1:
+        img.style.backgroundImage = "url(media/winter.jpg)";
+        body.style.setProperty("--main-color", "rgb(161, 227, 231)");
+        body.style.setProperty("--transparent-color", "rgba(93, 141, 153, 0.253)");
+        body.style.backgroundColor = "#000000";
+        break;
+    case 2: case 3: case 4:
+        img.style.backgroundImage = "url(media/spring.jpg)";
+        body.style.setProperty("--main-color", "rgb(219, 161, 231)");
+        body.style.setProperty("--transparent-color", "rgba(139, 93, 153, 0.253)");
+        break;
+    case 5: case 6: case 7:
+        img.style.backgroundImage = "url(media/summer.jpg)";
+        body.style.setProperty("--main-color", "rgb(80, 148, 85)");
+        body.style.setProperty("--transparent-color", "rgba(93, 153, 98, 0.253)");
+        break;
+    case 8: case 9: case 10:
+        img.style.backgroundImage = "url(media/autumn.jpg)";
+        body.style.setProperty("--main-color", "rgb(209, 103, 16)");
+        body.style.setProperty("--transparent-color", "rgba(153, 112, 93, 0.253)");
+        break;
+    default:
+        console.log("Błąd miesiąca dla: " + currentDate.getMonth);
+        break;
+}
 
-countdown.innerHTML = "Do urodzin zostało " + Math.ceil((birthdayDate.getTime() - currentDate.getTime())/1000/60/60/24) + " dni";
+let daysToBirthday = Math.ceil((birthdayDate.getTime() - currentDate.getTime())/1000/60/60/24);
+
+if (daysToBirthday === 0) {
+    countdown.innerHTML = "Wszystkiego najlepszego!";
+    img.style.backgroundImage = "url(media/birthday.jpg)";
+    body.style.setProperty("--main-color", "rgb(219, 219, 219)");
+    body.style.setProperty("--transparent-color", "rgba(204, 204, 204, 0.253)");
+} else if (daysToBirthday === 1){
+    countdown.innerHTML = "Do urodziń został: " + daysToBirthday + " dzień";
+} else {
+    countdown.innerHTML = "Do urodziń zostało: " + daysToBirthday + "dni";
+}
